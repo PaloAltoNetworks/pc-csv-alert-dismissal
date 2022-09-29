8ecf602df6292c505bcc5c81630160cd82f12e31
Adam <ahamiltonsut@paloaltonetworks.com>
2022-09-29 10:05:22 -0700

    Updated

--- README.md
+++ README.md
@@ -1,9 +1,32 @@
-## Installation
+# Overview
+
+This script accepts a CSV file that contains Alert IDs and Dismissal Reasons.
+
+The script groups Alerts by their Dismissal Reason and dismisses Alerts in batches.
+
+# Installation
 pip3 install loguru
 pip3 install pcpi
 
-## Running the script
+# Running the script
 
-python3 dismiss.py
+Defaults to file named "input.csv".
+```python3 dismiss.py```
 
-python3 dismiss.py -file inputfilename.csv
+Specify the path to the input csv using the "-file" option.
+```python3 dismiss.py -file inputfilename.csv```
+
+# Notes
+
+### One
+The script accepts a csv file with the following format:
+```Alert ID,Policy Name,dismissalNote```
+
+### Two
+The script is set to read credentials from a file by default. If the file does not exist, then the script will create the file for you.
+
+You can also have the script read from environment variables or directly from the user.
+
+Authentication with Prisma Cloud is handled by pcpi (Prisma Cloud Python Integration).
+Please refer to the GitHub page for this library for more details.
+[https://github.com/PaloAltoNetworks/pc-python-integration](https://github.com/PaloAltoNetworks/pc-python-integration)
--- dismiss.py
+++ dismiss.py
@@ -1,14 +1,30 @@
+from operator import index
 import loguru
 from pcpi import session_loader
 
 import sys
 
+
+def dismiss(alert_ids, dismissal_note):
+    payload = {
+        "alerts":alert_ids,
+        "dismissalNote":dismissal_note,
+        "filter":{
+            "timeRange":{"type":"to_now","value":"epoch"}
+        }
+    }
+    loguru_logger.info(f'Dismissing alerts with note: {dismissal_note}')
+    res = cspm_session.request('POST', '/alert/dismiss', json=payload)
+    if res.status_code == 200:
+        loguru_logger.info(f'Successfully dismissed {len(alert_ids)} alerts')
+
+
 loguru_logger = loguru.logger
 
-session_manager = session_loader.load_from_env(logger=loguru_logger)
+session_manager = session_loader.load_from_file(logger=loguru_logger)
 cspm_session = session_manager.create_cspm_session()
 
-filename = 'in.csv'
+filename = 'input.csv'
 if '-file' in sys.argv:
     filename = sys.argv[sys.argv.index('-file')+1]
 
@@ -17,27 +33,39 @@ if '.csv' not in filename:
     exit()
 
 #Open CSV and get alert IDs to dismiss
-alert_ids = []
+alerts = []
+counter = 0
 with open(filename, 'r') as infile:
     for line in infile:
-        if 'I-' not in line.split(',')[0]:
+        if counter == 0: #Skip headers line of file
+            counter+=1
             continue
+
         a_id = line.split(',')[0].strip()
-        print(a_id)
-        alert_ids.append(a_id)
+        d_note = line.split(',')[2].strip()
 
-payload = {
-    "alerts":alert_ids,
-    "dismissalNote":"DISMISSAL SCRIPT",
-    "filter":{
-        "timeRange":{"type":"to_now","value":"epoch"}
-    }
-}
+        alerts.append([a_id,d_note])
 
-loguru_logger.info(f'Dismissing alerts')
-res = cspm_session.request('POST', '/alert/dismiss', json=payload)
+        counter+=1
 
-if res.status_code == 200:
-    loguru_logger.info(f'Successfully dismissed {len(alert_ids)} alerts')
+#Break apart alerts based on dismissal note
+alert_ids = []
+chunk = []
+curr_note = alerts[0][1]
+index = 0
+offset = 0
+while index+offset < len(alerts):
+    while True:
+        alert_ids.append(alerts[offset+index][0])
+        index+=1
+        if offset+index >= len(alerts):
+            dismiss(alert_ids, curr_note)
+            break
 
-###
+        if curr_note != alerts[offset+index][1]:
+            dismiss(alert_ids, curr_note)
+            offset += index
+            index = 0
+            curr_note = alerts[offset+index][1]
+            alert_ids = []
+            break
